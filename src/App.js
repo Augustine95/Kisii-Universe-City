4d882e80aa26e505eb87f15c6f9f7ccae0b96559
Augustine Awuori <augustineawuori95@gmail.com>
2022-02-01 21:15:56 +0300

    Build the gadget component.

--- src/App.js
+++ src/App.js
@@ -1,23 +1,7 @@
 import avatar from './images/augustine.png'
 
 function App() {
-  return (
-    <div style={{ padding: '2rem' }}>
-      <article className="media">
-        <img
-          src={avatar}
-          className="media__image"
-          alt="There is nothing to show"
-        />
-        <div className="media__body">
-          <div className="media__title">Augustine Awuori</div>
-          <p className="media__content">
-            School of Information Science and Technology
-          </p>
-        </div>
-      </article>
-    </div>
-  )
+  return <div style={{ padding: '2rem' }}></div>
 }
 
 export default App
