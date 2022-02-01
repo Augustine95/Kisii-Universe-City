f84e09a9a90df33f9e1a58ba92604a3184a7f29e
Augustine Awuori <augustineawuori95@gmail.com>
2022-02-01 21:01:03 +0300

    Build the media componenet.

--- src/App.js
+++ src/App.js
@@ -1,7 +1,21 @@
+import avatar from './images/augustine.png'
+
 function App() {
   return (
     <div style={{ padding: '2rem' }}>
-      <button className="btn btn--primary btn--stretched">Post</button>
+      <article className="media">
+        <img
+          src={avatar}
+          className="media__image"
+          alt="There is nothing to show"
+        />
+        <div className="media__body">
+          <div className="media__title">Augustine Awuori</div>
+          <p className="media__content">
+            School of Information Science and Technology
+          </p>
+        </div>
+      </article>
     </div>
   )
 }
