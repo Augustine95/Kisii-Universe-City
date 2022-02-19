00601bae0791458398a2914041c8058d478d9567
Augustine Awuori <augustineawuori95@gmail.com>
2022-02-19 18:17:53 +0300

    Reimplement avatar to give letter if image is not provided.

--- src/components/avatar.jsx
+++ src/components/avatar.jsx
@@ -1,13 +1,51 @@
-import React from 'react'
+import React from "react";
+import Avatar from "@mui/material/Avatar";
 
-export default function Avatar({ src, className }) {
-  const getClassName = () => {
-    let name = 'avatar media__image '
-    if (className) name += className
-    return name
+const currentUser = { id: 1, name: "Augustine Awuroi", username: "@augustine" };
+
+const stringToColor = (string) => {
+  let hash = 0;
+  let i;
+
+  /* eslint-disable no-bitwise */
+  for (i = 0; i < string.length; i += 1) {
+    hash = string.charCodeAt(i) + ((hash << 5) - hash);
   }
 
+  let color = "#";
+
+  for (i = 0; i < 3; i += 1) {
+    const value = (hash >> (i * 8)) & 0xff;
+    color += `00${value.toString(16)}`.substr(-2);
+  }
+  /* eslint-enable no-bitwise */
+
+  return color;
+};
+
+function stringAvatar(name) {
+  return {
+    sx: {
+      bgcolor: stringToColor(name),
+    },
+    children: `${name.split(" ")[0][0]}${name.split(" ")[1][0]}`,
+  };
+}
+
+export default function AppAvatar({ src, className }) {
+  const getClassName = () => {
+    let name = "avatar media__image ";
+    if (className) name += className;
+    return name;
+  };
+
   return (
-    <img alt="Post" className={getClassName()} draggable="false" src={src} />
-  )
+    <Avatar
+      {...stringAvatar(currentUser.name)}
+      alt="Post"
+      className={getClassName()}
+      draggable="false"
+      src={src}
+    />
+  );
 }
